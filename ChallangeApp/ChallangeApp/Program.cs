4fad32e03ad78dcd7a03ddadea9ae5c2ea6cf20a
Łukasz <83137313+Fidek-creator@users.noreply.github.com>
2023-02-13 19:15:29 +0100

    Dzień 4

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "John";
+char sex = 'M';
+int age = 17;
+
+if(name == "John" || sex == 'M' || age >= 18)
+{
+    Console.WriteLine("Masz na imie john jestes mezczyzna i jestes niepelnoletni");
+}
